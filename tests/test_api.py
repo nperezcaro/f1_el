9a30eb8c346fd86705d1b5c5522b7cda013de061
Nicolas Perez Caro <nicolas.perez.caro@outlook.com>
2025-02-21 16:15:24 -0500

    test: add test to check the API's URL response and status

---
+++ tests/test_api.py
@@ -0,0 +1,15 @@
+from src.extract import get_yearly_sessions_data
+
+URL = "https://api.openf1.org/v1/sessions"
+
+
+def test_get_endpoint():
+    response = get_yearly_sessions_data(url=URL, year="2024")
+
+    assert (
+        response.status_code == 200
+    ), f"⛔ Expected status code 200, got {response.status_code} instead."
+
+    json_response = response.json()
+    assert isinstance(json_response, list), "⛔ Reponse must be a list of dicts."
+    assert "year" in json_response[0], "⛔ Reponse JSON does not contain year."
